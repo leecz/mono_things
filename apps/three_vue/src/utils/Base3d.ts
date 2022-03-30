6380cf17fd8a9a3e07979758f2bcc0875c8339d1
lichengzhi <lichengzhi@gz.sia.cn>
2022-03-31 00:28:21 +0800

    add three model

--- apps/three_vue/src/utils/Base3d.ts
+++ apps/three_vue/src/utils/Base3d.ts
@@ -1,7 +1,10 @@
 import * as THREE from "three"
 import { Camera, Object3D } from "three";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
-
+// 控制器
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+// 模型
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 class Base3d {
   // 初始属性没赋值，需要加!
@@ -9,6 +12,9 @@ class Base3d {
   scene!: THREE.Scene;
   renderer!: THREE.WebGLRenderer;
   camera!: THREE.PerspectiveCamera;
+
+  controls!: OrbitControls;
+  model!: Object3D;
 
   constructor(selector: string) {
     this.container = document.querySelector(selector) as HTMLElement
@@ -18,8 +24,25 @@ class Base3d {
   init() {
     this.initScene();
     this.initCamera();
+
     this.initRenderer()
+
+    //使用控制器
+    this.initControls()
+
+    // 添加物体
+    this.addMesh()
+
+    window.addEventListener('resize', this.onWindowResize.bind(this))
   }
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    // this.render()
+
+  }
+
   initScene() {
     this.scene = new THREE.Scene()
     this.setEnvMap()
@@ -31,10 +54,20 @@ class Base3d {
     )
     this.camera.position.set(-1.8, 0.6, 2.7)
   }
+  initControls() {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+  }
+
   initRenderer() {
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    // 设置屏幕像素比
     this.renderer.setPixelRatio(window.devicePixelRatio)
+
     this.renderer.setSize(window.innerWidth, window.innerHeight)
+
+    // 设置映射, 电影级别
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping
+    this.renderer.toneMappingExposure = 1
     this.container.appendChild(this.renderer.domElement)
   }
   setEnvMap() {
@@ -49,6 +82,20 @@ class Base3d {
   }
   animate() {
     this.renderer.setAnimationLoop(this.render.bind(this))
+  }
+  addMesh() {
+    this.setModel('/models/girl/', 'girl.gltf')
+    // this.setModel('/models/cathedral/', 'scene.gltf')
+  }
+  setModel(path: string, modelName: string) {
+    return new Promise((resolve, reject) => {
+      const loader = new GLTFLoader().setPath(path)
+      loader.load(modelName, (gltf) => {
+        this.model = gltf.scene
+        this.scene.add(gltf.scene)
+        resolve('ok')
+      })
+    })
   }
 
 
